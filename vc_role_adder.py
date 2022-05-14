588a4df6c569e806051962f55470742fee3526b3
Aryaan Sheth <avsheth03@gmail.com>
2022-05-14 11:16:11 -0400

    Main

--- vc_role_adder.py
+++ vc_role_adder.py
@@ -10,16 +10,15 @@ TOKEN = 'x'
 
 
 #ids should be a string of numbers
-vc_one = 0
-vc_two = 0
+vc_one, vc_two = 0, 0
 
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def on_ready() -> None:
+    print(f'We have logged in as {client}')
 
 # VC PROCESSING
 @client.event
-async def on_voice_state_update(member, before, after):
+async def on_voice_state_update(member, before, after) -> None:
     role = discord.utils.get(member.guild.roles, name="old role")
     current_role = discord.utils.get(member.guild.roles, name="new role")
     await member.remove_roles(role)
@@ -27,7 +26,7 @@ async def on_voice_state_update(member, before, after):
     
     if not before.channel and after.channel:
         channel_id = member.voice.channel.id
-        if channel_id == vc_one or channel_id == vc_two:
+        if channel_id in [vc_one, vc_two]: #== vc_one or channel_id == vc_two:
             await member.add_roles(current_role)
             
     elif before.channel and not after.channel:
